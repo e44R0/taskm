68abf031bfae586a313a158c40f30105501b0af6
daniilmarkelov <corppwork@gmail.com>
2025-10-14 20:10:19 +0300

    проверка бд

--- src/pages/api/projects/index.ts
+++ src/pages/api/projects/index.ts
@@ -1,19 +1,28 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { Project } from '@/types/project';
 import { getProjectsWithTags } from '@/db/project-service';
-import { authCheck } from '@/utils/utils';
+// import { authCheck } from '@/utils/utils';
+import cookie from "cookie";
+import {getSession} from "@/db/auth-service";
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Project[] | { message: string }>
 ) {
+  console.log('Projects --> Запросили проекты');
   if (req.method === 'GET') {
-    const auth = authCheck(req, res);
-    if (!auth) {
-      return;
-    }
 
-    const data: Project[] = getProjectsWithTags(auth.userId);
+    const parsedCookie = cookie.parse(req.headers.cookie ?? '');
+    const session = getSession(parsedCookie!.session!);
+    console.log('Projects --> session user id', session.userId);
+
+    // const auth = authCheck(req, res);
+    // if (!auth) {
+    //   return;
+    // }
+
+    const data: Project[] = getProjectsWithTags(session.userId);
+    console.log('Projects --> Запрос обработан')
 
     res.status(200).json(data);
   } else {
